0496899d16122d8e71f8137723f110746431cbd8
albertooherm <albertoherms17@gmail.com>
2024-02-08 00:10:57 -0600

    fix: modify structure navigation and eslint

---
+++ src/presentation/screens/DetailsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function DetailsScreen() {
+  return (
+    <View><Text>DetailScreen</Text></View>
+  )
+}
+
+export default DetailsScreen
